65a23f60a450fc0da33b21835702db81e9c21d22
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-13 18:38:02 +0530

    program uploaded

---
+++ for_loop.java
@@ -0,0 +1,11 @@
+class forloop
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i = 1;i<=5;i++)
+        {
+            System.out.println("hello world");
+        }
+    }
+}
