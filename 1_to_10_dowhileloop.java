99aed7be9c682315f8d8147a214e18b9974615db
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-13 18:58:03 +0530

    program uploaded

--- 1_to_10_dowhileloop.java
+++ 1_to_10_dowhileloop.java
@@ -3,11 +3,11 @@ class forloop
     public static void main(String args[])
     {
         int i=1;
-        while(i<=10)
+        do
         {
             System.out.println(i);
             i++;
 
-        }
+        }while(i<=10);
     }
 }
