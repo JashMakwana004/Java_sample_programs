3e470f9ee4f091b3d1b1850739af78d18b3c73d8
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-13 19:01:03 +0530

    program uploaded

---
+++ Alphabet_forloop.java
@@ -0,0 +1,11 @@
+class forloop
+{
+    public static void main(String args[])
+    {
+        char i;
+        for(i = 65;i<=90;i++)
+        {
+            System.out.println(" "+i+"");
+        }
+    }
+}
