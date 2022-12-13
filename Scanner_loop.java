532e6548253e85a8fa0cb71ab4fb7c65471d7b67
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-13 19:18:38 +0530

    program uploaded

---
+++ Scanner_loop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class forloop
+{
+    public static void main(String args[])
+    {
+        Scanner sin = new Scanner(System.in);
+        int n = sin.nextInt();
+        System.out.println("enter value : ")
+        int i;
+        for(i = 1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
