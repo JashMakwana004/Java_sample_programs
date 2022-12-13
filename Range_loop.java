b4c0687ad86fe3309b85223c33cb58990505a4b6
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-13 19:46:00 +0530

    program uploaded

---
+++ Range_loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class forloop
+{
+    public static void main(String args[])
+    {
+        Scanner sin = new Scanner(System.in);
+        int n = sin.nextInt();
+        int j = sin.nextInt();
+        System.out.println("enter value : ")
+        int i;
+        for(i = n;i<=j;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
