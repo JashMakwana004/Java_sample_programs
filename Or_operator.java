3d6c6051ac49c2f3916d2fe5239f85441b0eaecf
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-14 21:25:11 +0530

    program

---
+++ Or_operator.java
@@ -0,0 +1,14 @@
+class or_operator{
+       public static void main(String args[])
+       {
+        int a = 12 , b = 13 , c = 15;
+        if(a == 15||b == 15||c == 15)
+        {
+            System.out.println("we got 15");
+        }
+        else
+        {
+            System.out.println("no 15");
+        }
+       }
+}
