1955d1d21db138d41b1afdd04fe254b6f3202f9e
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-14 17:52:46 +0530

    program

---
+++ Swapping_two_with_variable.java
@@ -0,0 +1,16 @@
+class swapping
+{
+    public static void main(String args[])
+    {
+        int i = 1;
+        int j = 2;
+        int c;
+        c = j;
+        j = i;
+        i = c;
+        
+        System.out.println(i);
+        System.out.println(j);
+
+    }
+}
