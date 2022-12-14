368f8d99e40c67e6900d15d60eeb8f99c87e48e4
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-14 18:00:38 +0530

    program

---
+++ Swapping_two_without_variable.java
@@ -0,0 +1,16 @@
+class pattern
+{
+    public static void main(String args[])
+    {
+        int i = 1;
+        int j = 2;
+        j = j - i;
+        i = i + j;
+        System.out.println(i);
+        System.out.println(j);
+
+
+
+
+    }
+}
