0f7d4550625794ae97e46e68f930afa393757006
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-14 18:27:46 +0530

    program

---
+++ If_else_using_scanner_class.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class ifelse
+{
+    public static void main(String args[])
+    {
+        Scanner sin = new Scanner(System.in);
+        int i = sin.nextInt();
+        if(i == 1)
+        {
+            System.out.println("a is true");
+
+        }
+        else
+        {
+            System.out.println("a is false");
+        }
+    }
+}
