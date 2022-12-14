02c9215a4281f23c6e87ae8fc14f8f110236f9a1
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-14 18:14:15 +0530

    program

---
+++ If_else_simple_condition.java
@@ -0,0 +1,16 @@
+class ifelse
+{
+    public static void main(String args[])
+    {
+        int i = 1;
+        if(i == 1)
+        {
+            System.out.println("a is true");
+
+        }
+        else
+        {
+            System.out.println("a is false");
+        }
+    }
+}
