064f3e93ff95b32b64ee7458893eeff45db176c0
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-17 19:31:49 +0530

    program

---
+++ Array_sample.java
@@ -0,0 +1,11 @@
+class Array_sample
+{
+    public static void main(String args[])
+    {
+        int a[] = {1,2,3,4,5}
+        for(int a1 : a)
+        {
+            System.out.println(a1);
+        }
+    }
+}
