e2a0d737ad96be5e2743956d58615c3d864f4c68
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-17 22:04:57 +0530

    program uploaded

---
+++ Array_sample_2_method.java
@@ -0,0 +1,11 @@
+class Array
+{
+    public static void main(String args[])
+    {
+        int a[] = new int[5];
+        for(int a1 :a)
+        {
+            System.out.println(a1);
+        }
+    }
+}
