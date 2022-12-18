e0e70ba9eff28c027ece133ab84ce2139860b062
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-18 16:57:47 +0530

    infinite loop

---
+++ Infinite_scanner_loop.java
@@ -0,0 +1,17 @@
+class forloop
+{
+    public static void main(String args[])
+    {
+       Scanner sin = new Scanner(System.in);
+        for(;;)
+        {
+            int n = sin.nextInt();
+            System.out.println(i);
+            if(n == 0)
+            {
+                break;
+            }
+        }
+        System.out.println("over");
+    }
+}
