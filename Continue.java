3b393878c3fafd8c877f0d0e9ffa1366f4fb0837
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-18 16:52:59 +0530

    continue

---
+++ Continue.java
@@ -0,0 +1,16 @@
+class forloop
+{
+    public static void main(String args[])
+    {
+       int i;
+        for(i = 1;i<=10;i++)
+        {
+            if(i == 4)
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+        System.out.println("over");
+    }
+}
