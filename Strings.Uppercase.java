0f24c323c8e4c30aba7ed847673a9dbea4cdedd8
JashMakwana004 <atul.makwana76@gmail.com>
2022-12-27 22:33:06 +0530

    practise

---
+++ Strings.Uppercase.java
@@ -0,0 +1,11 @@
+class forloop
+{
+    public static void main(String args[])
+    {
+        String s = new String ("pqr");
+        String s1 = "abc";
+        String s2 = s1.toUpperCase();
+        System.out.println(s2);
+        
+    }
+}
