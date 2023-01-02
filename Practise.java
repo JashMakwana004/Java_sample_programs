b2c8a73791eec215a045cde5cd1b191c925597db
JashMakwana004 <atul.makwana76@gmail.com>
2023-01-02 20:41:12 +0530

    practises

---
+++ Practise.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class forloop
+{
+    public static void main(String args[])
+    {
+        
+        int a[]={1,2,3,4};
+        int b[4] = {5,6,7,8};
+        System.out.println(a[0]+","+b[0]); 
+        //int n = sin.nextInt();
+        /*StringBuffer sb = new StringBuffer("abc");
+        sb.append("pqr");
+        System.out.println(sb);
+        System.out.println(sb.insert(3,"*"));
+        System.out.println(sb.replace(3,5,"*"));  
+        System.out.println(sb.delete(3,4));
+        //System.out.println(sb.reverse());
+        System.out.println(sb.capacity()); */
+
+    }
+}
