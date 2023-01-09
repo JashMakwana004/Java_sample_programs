909a362acb9fa3fed693beb0b1c929563a6c3a47
JashMakwana004 <atul.makwana76@gmail.com>
2023-01-09 20:52:11 +0530

    sum_avg_by_arrays

---
+++ sum_min_max_avg.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class forloop
+{
+    public static void main(String args[])
+    {
+        Scanner sin =new Scanner(System.in);
+        int n = sin.nextInt();
+        int a[] = new int[n];
+        for(int i = 0;i<a.length;i++)
+        {
+            a[i] = sin.nextInt();
+        }
+        int sum = 0;int min = a[0]; int max = a[0];
+        for(int i = 0;i<a.length;i++)
+        {
+            sum = sum + a[i];
+            if(min>a[i])
+            {
+                min = a[i];
+
+            }
+            if(max<a[i])
+            {
+                max = a[i];
+            }
+        }
+        System.out.println("avg"+sum/a.length);
+        System.out.println("sum"+sum);
+        System.out.println("min"+min);
+        System.out.println("max"+max);
+    }
+}
