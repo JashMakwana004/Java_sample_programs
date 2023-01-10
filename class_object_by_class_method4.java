968d8bd4c170fa1c8bdda2b11e7964f3f5877388
JashMakwana004 <atul.makwana76@gmail.com>
2023-01-10 21:07:54 +0530

    Volume_by_object_class_method4

---
+++ class_object_by_class_method4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Boxdemo
+{
+    public static void main(String args[])
+    {
+        Box b = new Box();
+        System.out.println(b.volume());
+
+    }
+}
+class Box{
+    int length = 10;
+    int breadth = 10;
+    int heigth = 10;
+    public int volume()
+    {
+        //int vol = length*breadth*heigth;
+        //System.out.println(vol);
+        return(length*breadth*heigth);
+    }
+}
