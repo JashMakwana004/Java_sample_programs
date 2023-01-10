2d7c5ac8ad749a6ae852a096b54c9a8bcf30a372
JashMakwana004 <atul.makwana76@gmail.com>
2023-01-10 19:44:34 +0530

    program

---
+++ class_object_demo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class forloop
+{
+    public static void main(String args[])
+    {
+        String str =new String("hello");
+        System.out.println(str);
+        String str1 = new String("world");
+        System.out.println(str1);
+        String str2 = str1;
+        str1 = null;
+        System.out.println(str2);
+        System.out.println(str1);
+    }
+}
