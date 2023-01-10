1cef5abd3fc2a6a21762c919ce5fe63a6230170c
JashMakwana004 <atul.makwana76@gmail.com>
2023-01-10 22:39:25 +0530

    two_variable_scanner

---
+++ class_object_by_scanner_class.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Boxdemo
+{
+    public static void main(String args[])
+    {
+        Scanner sin = new Scanner(System.in);
+
+        Box b1 = new Box();
+        b1.length = sin.nextInt();
+        b1.breadth = sin.nextInt();
+        b1.height = sin.nextInt();
+        b1.volume();
+
+    }
+}
+class Box{
+    int length;
+    int breadth;
+    int height;
+    public void volume()
+    {
+        
+        System.out.println(length*breadth*heigth;);
+    }
+}
